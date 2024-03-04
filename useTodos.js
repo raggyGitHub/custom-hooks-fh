626449181f2dbffed0971e29b58bd2805304ac9e
ronald sarmiento <ronisarmont@gmail.com>
2024-03-04 17:13:26 -0400

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,50 @@
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer/todoReducer';
+
+export const useTodos = () => {
+  const initialState = [];
+
+  const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+  };
+
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    // console.log(todos);
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    // console.log({ todo });
+    const action = {
+      type: '[TODO] Add Todo',
+      payload: todo,
+    };
+
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: '[TODO] Remove Todo',
+      payload: id,
+    });
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch({
+      type: '[TODO] Toggle Todo',
+      payload: id,
+    });
+  };
+
+  return {
+    todos,
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter((todo) => !todo.done).length,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+  };
+};
